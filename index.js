06300824f38d1d1d499a6c8eb994fbf5c9295901
George <gwwebster23@gmail.com>
2023-01-29 17:08:16 -0600

    loop tests passing

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+const cards = []; 
+const message = [];
+
+function writeCards(array, event) {
+    for (let i = 0; i < array.length; i++) {
+        cards.push(`Thank you, ${array[i]}, for the wonderful surprise gift!`);
+    }   
+    return cards;
+}
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number);
+        number--;
+    }
+}
